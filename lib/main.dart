19b72dfb61678ce11ba79bc1172318cf23fd1857
Sreekanth <kithuv21@gmail.com>
2025-06-05 21:44:36 +0530

    minor

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:lawtus/homepage.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_app_check/firebase_app_check.dart';
+
 import 'firebase_options.dart';
-import 'package:flutter/foundation.dart';
+
 
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
   try
 {  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,);
-   await FirebaseAppCheck.instance.activate(
-    androidProvider: AndroidProvider.debug
-   );
-   print('App is checked');
 }catch(e){
-  print('pleaseeee$e');
+  print(e);
 }
   runApp(const MyApp());
 }
